ea74f19417e9c654bbc130b18d8b0cbeaf560650
jellyPark <znba1004@gmail.com>
2018-03-19 15:51:20 +0900

    add getmapping

--- src/main/java/com/lush/core/controllers/Actuator.java
+++ src/main/java/com/lush/core/controllers/Actuator.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -105,7 +106,7 @@ public class Actuator {
    * @return ResponseEntity
    * @throws UnknownHostException
    */
-  // @GetMapping("/")
+  @GetMapping("/")
   public ResponseEntity<Object> endpoints() throws UnknownHostException {
     // Get endpoints data.
     String uri = setUri("mappings");
